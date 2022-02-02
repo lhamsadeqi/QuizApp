0fa6996db4eb0f21bc5d5dd1f3982598789c193d
lhamsadeqi <elhamsadeqi.es@gmail.com>
2022-02-02 15:17:39 +0330

    splitting into widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
-// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors, unused_local_variable, must_be_immutable, avoid_print
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors, unused_local_variable, must_be_immutable, avoid_print, prefer_final_fields
 
 import 'package:flutter/material.dart';
-import 'package:quiz/Btn/answer_button.dart';
-import 'package:quiz/widget/question.dart';
+
+import './widget/quiz.dart';
+import './widget/result.dart';
 
 void main() => runApp(MyApp());
 
@@ -12,69 +13,76 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  var _questions = [
+    {
+      'Q': 'Who is your favorite singre?',
+      'A': [
+        'Taylor Swift',
+        'Cavetown',
+        'Lord Huron',
+        'Alex Turner',
+        'none.',
+      ],
+    },
+    {
+      'Q': 'What kind of music do you listen to?',
+      'A': [
+        'Pop',
+        'Alternative',
+        'Jazz',
+        'Indie',
+        'Rock',
+      ],
+    },
+    {
+      'Q': 'Choose a band.',
+      'A': [
+        'Archive',
+        'The Strokes',
+        'The Beatles',
+        'Arctic Monkeys',
+        'none.',
+      ],
+    },
+    {
+      'Q': 'Your Favorite song by Taylor Swift?',
+      'A': [
+        'Seven',
+        'All Too Well',
+        'Sad Beautiful Tragic',
+        'Champagne Problems',
+        'I donno these songs :( ',
+      ],
+    },
+    {
+      'Q': 'Which one is your guilty pleasure?',
+      'A': [
+        'Kpop',
+        'Justin Bieber',
+        'Persian Pop',
+        'One Direction',
+        'I listen to them normally.',
+      ],
+    },
+  ];
+
+//...
   var _questionIndex = 0;
 
   void _answerQuestion() {
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
+    print(_questionIndex);
+    if (_questionIndex < _questions.length) {
+      print('We have more questions');
+    } else {
+      print('No more');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      {
-        'Q': 'Who is your favorite singre?',
-        'A': [
-          'Taylor Swift',
-          'Cavetown',
-          'Lord Huron',
-          'Alex Turner',
-          'none.',
-        ],
-      },
-      {
-        'Q': 'What kind of music do you listen to?',
-        'A': [
-          'Pop',
-          'Alternative',
-          'Jazz',
-          'Indie',
-          'Rock',
-        ],
-      },
-      {
-        'Q': 'Choose a band.',
-        'A': [
-          'Archive',
-          'The Strokes',
-          'The Beatles',
-          'Arctic Monkeys',
-          'none.',
-        ],
-      },
-      {
-        'Q': 'Your Favorite song by Taylor Swift?',
-        'A': [
-          'Seven',
-          'All Too Well',
-          'Sad Beautiful Tragic',
-          'Champagne Problems',
-          'I donno these songs :( ',
-        ],
-      },
-      {
-        'Q': 'Which one is your guilty pleasure?',
-        'A': [
-          'Kpop',
-          'Justin Bieber',
-          'Persian Pop',
-          'One Direction',
-          'I listen to them normally.',
-        ],
-      },
-    ];
-
     //...
     return MaterialApp(
       home: Scaffold(
@@ -83,22 +91,13 @@ class _MyAppState extends State<MyApp> {
           centerTitle: true,
           title: Text('Vibe Check'),
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          // ignore: prefer_const_literals_to_create_immutables
-          children: [
-            Qusetion(
-              questions[_questionIndex]['Q'].toString(),
-            ),
-            SizedBox(height: 25),
-            ...(questions[_questionIndex]['A'] as List).map((answer) {
-              return AnswerButton(
-                txt: answer,
-                answer: _answerQuestion,
-              );
-            }).toList(),
-          ],
-        ),
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                questions: _questions,
+                questionIndex: _questionIndex,
+                answerQuestion: _answerQuestion,
+              )
+            : Result(),
       ),
     );
   }
